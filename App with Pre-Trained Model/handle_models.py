f22087d71a74ebdc8479e4265c9ec51b1b576be9
Rajneesh Kumar <rajneeshrooney@gmail.com>
2019-12-26 15:57:46 +0530

    Update handle_models.py

--- App with Pre-Trained Model/handle_models.py
+++ App with Pre-Trained Model/handle_models.py
@@ -7,10 +7,10 @@ def handle_pose(output, input_shape):
     Handles the output of the Pose Estimation model.
     Returns ONLY the keypoint heatmaps, and not the Part Affinity Fields.
     '''
-    #Extract only the second blob output (keypoint heatmaps)
+    # Extract only the second blob output (keypoint heatmaps)
     heatmaps = output['Mconv7_stage2_L2']
-    #Resize the heatmap back to the size of the input
-    #create an empty array to handle the output map
+    # Resize the heatmap back to the size of the input
+    # Create an empty array to handle the output map
     out_heatmap = np.zeros([heatmaps.shape[1], input_shape[0], input_shape[1]])
     # Iterate through and re-size each heatmap
     for h in range(len(heatmaps[0])):
@@ -18,41 +18,39 @@ def handle_pose(output, input_shape):
 
     return out_heatmap
 
-   
+
 def handle_text(output, input_shape):
     '''
     Handles the output of the Text Detection model.
     Returns ONLY the text/no text classification of each pixel,
         and not the linkage between pixels and their neighbors.
     '''
-    #Extract only the first blob output (text/no text classification)
+    # Extract only the first blob output (text/no text classification)
     text_classes = output['model/segm_logits/add']
-    #Resize this output back to the size of the input
+    # Resize this output back to the size of the input
     out_text = np.empty([text_classes.shape[1], input_shape[0], input_shape[1]])
     for t in range(len(text_classes[0])):
         out_text[t] = cv2.resize(text_classes[0][t], input_shape[0:2][::-1])
 
     return out_text
 
-    
+
 def handle_car(output, input_shape):
     '''
     Handles the output of the Car Metadata model.
     Returns two integers: the argmax of each softmax output.
     The first is for color, and the second for type.
     '''
-    #Get the argmax of the "color" output
-    #Get the argmax of the "type" output
     # Get rid of unnecessary dimensions
     color = output['color'].flatten()
     car_type = output['type'].flatten()
-    #Get the argmax of the "color" output
+    #  Get the argmax of the "color" output
     color_pred = np.argmax(color)
-    #Get the argmax of the "type" output
+    #  Get the argmax of the "type" output
     type_pred = np.argmax(car_type)
 
     return color_pred, type_pred
-    
+
 
 def handle_output(model_type):
     '''
@@ -70,11 +68,16 @@ def handle_output(model_type):
 
 
 '''
-You need to call it  in `app.py` to preprocess
+You just need to call the function in `app.py` to preprocess
 the input image.
 '''
 def preprocessing(input_image, height, width):
-  
+    '''
+    Given an input image, height and width:
+    - Resize to width and height
+    - Transpose the final "channel" dimension to be first
+    - Reshape the image to add a "batch" of 1 at the start 
+    '''
     image = np.copy(input_image)
     image = cv2.resize(image, (width, height))
     image = image.transpose((2,0,1))
